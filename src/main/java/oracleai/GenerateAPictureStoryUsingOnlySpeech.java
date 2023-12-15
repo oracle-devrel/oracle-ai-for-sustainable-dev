3da51e45d4eea02946df9b6ee48a815048e429fe
Paul Parkinson <paul.parkinson@gmail.com>
2023-12-15 14:25:51 -0500

    init 3

--- src/main/java/oracleai/GenerateAPictureStoryUsingOnlySpeech.java
+++ src/main/java/oracleai/GenerateAPictureStoryUsingOnlySpeech.java
@@ -1,8 +1,11 @@
 package oracleai;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import oracleai.services.ImageGeneration;
 import oracleai.services.OracleObjectStore;
 import oracleai.services.OracleSpeechAI;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.sound.sampled.*;
 import java.io.*;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/picturestory")
@@ -26,25 +30,32 @@ public class GenerateAPictureStoryUsingOnlySpeech {
     }
 
     @PostMapping("/picturestory")
-    public String picturestory(@RequestParam("genopts") String genopts,
-                               @RequestParam("file") MultipartFile multipartFile, Model model) throws Exception {
+    public String picturestory(@RequestParam("opts") String opts,
+                               @RequestParam("genopts") String genopts,
+                               @RequestParam("file") MultipartFile multipartFile,
+                               Model model) throws Exception {
+        if (opts.equals("fileaudio") ) return fileaudio(genopts, multipartFile, model);
+        else return liveaudio(genopts, model);
+    }
+
+    @NotNull
+    private String fileaudio(String genopts, MultipartFile multipartFile, Model model) throws Exception {
         OracleObjectStore.sendToObjectStorage(multipartFile.getOriginalFilename(), multipartFile.getInputStream());
         String transcriptionJobId = OracleSpeechAI.getTranscriptFromOCISpeech(multipartFile.getOriginalFilename());
         System.out.println("transcriptionJobId: " + transcriptionJobId);
         String jsonTranscriptFromObjectStorage =
                 OracleObjectStore.getFromObjectStorage(transcriptionJobId,
-                        AIApplication.OBJECTSTORAGE_NAMESPACE + "_" + AIApplication.OBJECTSTORAGE_BUCKETNAME + "_" + multipartFile.getOriginalFilename() + ".json");
+                        AIApplication.OBJECTSTORAGE_NAMESPACE + "_" +
+                                AIApplication.OBJECTSTORAGE_BUCKETNAME + "_" +
+                                multipartFile.getOriginalFilename() + ".json");
         System.out.println("jsonTranscriptFromObjectStorage: " + jsonTranscriptFromObjectStorage);
-//        System.out.println("getFromObjectStorage: " + getFromObjectStorage("leia.m4a"));
-//        String pictureDescription  = parse(jsonTranscriptFromObjectStorage);
-        String pictureDescription = "man rowing a boat through the forest";
+        String pictureDescription  = getConcatenatedTokens(jsonTranscriptFromObjectStorage);
         imageLocations.add(ImageGeneration.imagegeneration(pictureDescription + " " + genopts));
         model.addAttribute("imageLocations", imageLocations.toArray(new String[0]));
         return "resultswithimages";
     }
 
-    @GetMapping("/picturestoryrecordlocally")
-    public String picturestoryrecordlocally(@RequestParam("genopts") String genopts, Model model) throws Exception {
+    public String liveaudio(String genopts, Model model) throws Exception {
         AudioFormat format =
                 new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, 44100.0f, 16, 1,
                         (16 / 8) * 1, 44100.0f, true);
@@ -65,24 +76,33 @@ public class GenerateAPictureStoryUsingOnlySpeech {
         AudioSystem.write(audioInputStream, fileType, file);
         System.out.println("Saved " + file.getAbsolutePath());
         OracleObjectStore.sendToObjectStorage(file.getName(), new FileInputStream(file));
-
-        String transcriptionJobId = OracleSpeechAI.getTranscriptFromOCISpeech(file.getName());  //json file
-//        String transcriptionJobId = getTranscriptFromOCISpeech("testing123.wav");
+        String transcriptionJobId = OracleSpeechAI.getTranscriptFromOCISpeech(file.getName());
         System.out.println("transcriptionJobId: " + transcriptionJobId);
         String jsonTranscriptFromObjectStorage =
                 OracleObjectStore.getFromObjectStorage(transcriptionJobId,
-//                        AIApplication.OBJECTSTORAGE_NAMESPACE + "_" + AIApplication.OBJECTSTORAGE_BUCKETNAME + "_" + "testing123.wav" + ".json"));
-                        AIApplication.OBJECTSTORAGE_NAMESPACE + "_" + AIApplication.OBJECTSTORAGE_BUCKETNAME + "_" + file.getName() + ".json");
+                        AIApplication.OBJECTSTORAGE_NAMESPACE + "_" +
+                                AIApplication.OBJECTSTORAGE_BUCKETNAME + "_" + file.getName() + ".json");
         System.out.println("jsonTranscriptFromObjectStorage: " + jsonTranscriptFromObjectStorage);
-//        System.out.println("getFromObjectStorage: " + getFromObjectStorage("leia.m4a"));
-//        String pictureDescription  = parse(jsonTranscriptFromObjectStorage);
-        String pictureDescription = "man rowing a boat through the forest";
+        String pictureDescription  = getConcatenatedTokens(jsonTranscriptFromObjectStorage);
         imageLocations.add(ImageGeneration.imagegeneration(pictureDescription + " " + genopts));
         model.addAttribute("imageLocations", imageLocations.toArray(new String[0]));
         return "resultswithimages";
     }
 
-
+    public String getConcatenatedTokens(String json) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            OracleSpeechAI.TranscriptionResponse response =
+                    objectMapper.readValue(json, OracleSpeechAI.TranscriptionResponse.class);
+            return response.getTranscriptions().stream()
+                    .flatMap(transcription -> transcription.getTokens().stream())
+                    .map(OracleSpeechAI.TranscriptionResponse.Transcription.Token::getToken)
+                    .collect(Collectors.joining(" "));
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 
     public class SoundRecorder implements Runnable {
         AudioInputStream audioInputStream;
@@ -106,7 +126,8 @@ public class GenerateAPictureStoryUsingOnlySpeech {
 
         @Override
         public void run() {
-            try (final ByteArrayOutputStream out = new ByteArrayOutputStream(); final TargetDataLine line = getTargetDataLineForRecord();) {
+            try (final ByteArrayOutputStream out = new ByteArrayOutputStream();
+                 final TargetDataLine line = getTargetDataLineForRecord();) {
                 int frameSizeInBytes = format.getFrameSize();
                 int bufferLengthInFrames = line.getBufferSize() / 8;
                 final int bufferLengthInBytes = bufferLengthInFrames * frameSizeInBytes;
@@ -121,7 +142,9 @@ public class GenerateAPictureStoryUsingOnlySpeech {
             }
         }
 
-        public void buildByteOutputStream(final ByteArrayOutputStream out, final TargetDataLine line, int frameSizeInBytes, final int bufferLengthInBytes) throws IOException {
+        public void buildByteOutputStream(final ByteArrayOutputStream out,
+                                          final TargetDataLine line, int frameSizeInBytes,
+                                          final int bufferLengthInBytes) throws IOException {
             final byte[] data = new byte[bufferLengthInBytes];
             int numBytesRead;
 
