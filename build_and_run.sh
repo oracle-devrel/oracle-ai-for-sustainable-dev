ffa99402d48e42714cc447b94d1dc9eb6871bbce
Paul Parkinson <paul.parkinson@gmail.com>
2023-12-15 20:23:55 -0500

    init 3

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#source ~/Downloads/env.properties
 export `cat env.properties`
+#export `cat ~/Downloads/env.properties`
 
 mvn clean package ; java -jar target/oracleai-0.0.1-SNAPSHOT.jar
