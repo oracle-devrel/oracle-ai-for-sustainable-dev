85e9b9e49a2b97c1bd9174656dee9c908ccd44ba
Paul Parkinson <paul.parkinson@oracle.com>
2024-08-15 23:11:41 -0400

    Update init_rag.py

--- python-rag-chatbot/init_rag.py
+++ python-rag-chatbot/init_rag.py
@@ -49,7 +49,7 @@ from config_rag import (
 
 # private configs
 CONFIG_PROFILE = "DEFAULT"
-COMPARTMENT_OCID = "ocid1.compartment.oc1..aaaaaaaajdyhd7dqnix2avhlckbhhkkcl3cujzyuz6jzyzonadca3i66pqjq"
+COMPARTMENT_OCID = "ocid1.compartment.yourcomparmentid"
 oci_config = oci.config.from_file("~/.oci/config", CONFIG_PROFILE)
 COHERE_API_KEY = oci_config['security_token_file']
 DEBUG = True
