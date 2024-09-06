cd2229dc39088ffe860fda50df85fc914da6f18b
paulparkinson <paul.parkinson@gmail.com>
2024-09-06 12:34:24 -0400

    digital double logic

--- java-ai/src/main/java/oracleai/services/AuthProvider.java
+++ java-ai/src/main/java/oracleai/services/AuthProvider.java
@@ -6,21 +6,19 @@ import com.oracle.bmc.auth.AuthenticationDetailsProvider;
 import com.oracle.bmc.auth.BasicAuthenticationDetailsProvider;
 import com.oracle.bmc.auth.ConfigFileAuthenticationDetailsProvider;
 import com.oracle.bmc.auth.ResourcePrincipalAuthenticationDetailsProvider;
+import com.oracle.bmc.auth.InstancePrincipalsAuthenticationDetailsProvider;
 import java.io.IOException;
 
 public class AuthProvider {
 
     public static BasicAuthenticationDetailsProvider getAuthenticationDetailsProvider() throws IOException {
-        return ResourcePrincipalAuthenticationDetailsProvider.builder().build();
+        if (isRunningInOKE()) return InstancePrincipalsAuthenticationDetailsProvider.builder().build();
+        else return new ConfigFileAuthenticationDetailsProvider(
+                System.getenv("OCICONFIG_FILE"), System.getenv("OCICONFIG_PROFILE"));
     }
 
     private static boolean isRunningInOKE() {
         return true; //System.getenv("OCI_RESOURCE_PRINCIPAL_VERSION") != null;
     }
 
-    public static AuthenticationDetailsProvider getConfigFileAuthenticationDetailsProvider() throws IOException {
-        return new ConfigFileAuthenticationDetailsProvider(
-                    System.getenv("OCICONFIG_FILE"), System.getenv("OCICONFIG_PROFILE"));
-    //    InstancePrincipalsAuthenticationDetailsProvider.builder().build();
-    }
 }
