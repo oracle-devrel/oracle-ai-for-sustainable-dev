9239c1fd231b80aaa75de64e56e5767172053f91
paulparkinson <paul.parkinson@gmail.com>
2024-09-07 19:53:41 -0400

    digital double logic

--- java-ai/src/main/java/oracleai/DigitalDoubleService.java
+++ java-ai/src/main/java/oracleai/DigitalDoubleService.java
@@ -1,12 +1,9 @@
 package oracleai;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -23,24 +20,27 @@ public class DigitalDoubleService {
 
     public void updateDigitalDoubleData(DigitalDoubleDownloadInfo info) {
         String url = AIApplication.ORDS_OMLOPSENDPOINT_URL + "update_digital_double_data/";
+
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
-        Map<String, Object> requestBody = new HashMap<>();
+
+        Map<String, String> requestBody = new HashMap<>();
         requestBody.put("p_participant_email", info.getEmail());
         requestBody.put("p_modelglburl_out", info.getModelGlbUrl());
-        requestBody.put("p_modelfbxurl_out", info.getModelFbxUrl());
-        requestBody.put("p_modelusdzurl_out", info.getModelUsdzUrl());
-        requestBody.put("p_thumbnailurl_out", info.getThumbnailUrl());
-        requestBody.put("p_videourl_out", info.getAnimatedVideoLocation());
-        requestBody.put("p_video_out", "");
-        requestBody.put("p_similar_image_out", "");
-        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(requestBody, headers);
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> response = restTemplate.postForEntity(url, entity, String.class);
+        requestBody.put("p_modelfbxurl_out", info.modelFbxUrl);
+        requestBody.put("p_modelusdzurl_out", info.modelUsdzUrl);
+        requestBody.put("p_thumbnailurl_out", info.thumbnailUrl);
+
+        HttpEntity<Map<String, String>> entity = new HttpEntity<>(requestBody, headers);
+
+        ResponseEntity<String> response =
+                new RestTemplate().exchange(url, HttpMethod.POST, entity, String.class);
+
         if (response.getStatusCode().is2xxSuccessful()) {
             System.out.println("Request successful: " + response.getBody());
         } else {
             System.err.println("Request failed with status code: " + response.getStatusCode());
         }
+
     }
 }
