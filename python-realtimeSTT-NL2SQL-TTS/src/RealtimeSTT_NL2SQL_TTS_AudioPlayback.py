809dc0e7c782acef63242c755d518bdb346602f9
paulparkinson <paul.parkinson@gmail.com>
2025-02-02 15:18:11 -0500

    update requirements.txt and add functionality for all select ai actions

--- python-realtimeSTT-NL2SQL-TTS/src/RealtimeSTT_NL2SQL_TTS_AudioPlayback.py
+++ python-realtimeSTT-NL2SQL-TTS/src/RealtimeSTT_NL2SQL_TTS_AudioPlayback.py
@@ -47,11 +47,17 @@ FRAMES_PER_BUFFER = int(SAMPLE_RATE * BUFFER_DURATION_MS / 1000)
 
 cummulativeResult = ""
 isSelect = False
+isNarrate = False
+isShowSQL = False
+isRunSQL = False
+isExplainSQL = False
 last_result_time = None
+
 
 def audio_callback(in_data, frame_count, time_info, status):
     queue.put_nowait(in_data)
     return (None, pyaudio.paContinue)
+
 
 p = pyaudio.PyAudio()
 
@@ -68,10 +74,12 @@ stream.start_stream()
 config = from_file()
 isInsertResults = False
 
+
 async def send_audio(client):
     while True:
         data = await queue.get()
         await client.send_data(data)
+
 
 def play_audio(file_path):
     try:
@@ -96,9 +104,10 @@ def play_audio(file_path):
     except Exception as e:
         print(f"Error playing audio: {e}")
 
+
 class SpeechListener(RealtimeClientListener):
     def on_result(self, result):
-        global cummulativeResult, isSelect, last_result_time
+        global cummulativeResult, isSelect, isNarrate, isShowSQL, isRunSQL, isExplainSQL, last_result_time
         if result["transcriptions"][0]["isFinal"]:
             transcription = result['transcriptions'][0]['transcription']
             cummulativeResult += transcription
@@ -107,41 +116,60 @@ class SpeechListener(RealtimeClientListener):
             if cummulativeResult.lower().startswith("hey db"):
                 cummulativeResult = cummulativeResult[len("hey db"):].strip()
                 isSelect = True
+            elif cummulativeResult.lower().startswith("hey deebee"):
+                cummulativeResult = cummulativeResult[len("hey deebee"):].strip()
+                isSelect = True
             elif cummulativeResult.lower().startswith("adb"):
                 cummulativeResult = cummulativeResult[len("adb"):].strip()
                 isSelect = True
             else:
                 cummulativeResult = ""
+            if cummulativeResult.lower().endswith("use narrate"):
+                cummulativeResult = cummulativeResult[:-len("use narrate")].strip()
+                isNarrate = True
+                print(f"isNarrate: {isNarrate}")
+            elif cummulativeResult.lower().endswith("use show sql"):
+                cummulativeResult = cummulativeResult[:-len("use show sql")].strip()
+                isShowSQL = True
+                print(f"isShowSQL: {isShowSQL}")
+            elif cummulativeResult.lower().endswith("use run sql"):
+                cummulativeResult = cummulativeResult[:-len("use run sql")].strip()
+                isRunSQL = True
+                print(f"isRunSQL: {isRunSQL}")
+            elif cummulativeResult.lower().endswith("use explain"):
+                cummulativeResult = cummulativeResult[:-len("use explain sql")].strip()
+                isExplainSQL = True
+                print(f"isExplainSQL: {isExplainSQL}")
             last_result_time = asyncio.get_event_loop().time()
         else:
             print(f"Received partial results: {result['transcriptions'][0]['transcription']}")
 
     def on_ack_message(self, ackmessage):
-        """Handle acknowledgment messages (required by the abstract class)."""
         print(f"ACK received: {ackmessage}")
 
     def on_connect(self):
-        """Handle connection event (required by the abstract class)."""
         print("Connected to Realtime Speech Service.")
 
     def on_connect_message(self, connectmessage):
-        """Handle connection messages (required by the abstract class)."""
         print(f"Connect message: {connectmessage}")
 
     def on_network_event(self, ackmessage):
-        """Handle network events (required by the abstract class)."""
         print(f"Network event: {ackmessage}")
 
     def on_error(self, exception):
-        """Handle errors (required by the abstract class)."""
         print(f"An error occurred: {exception}")
 
+
 async def check_idle():
-    global last_result_time, isSelect
+    global last_result_time, isSelect, isNarrate, isShowSQL, isRunSQL, isExplainSQL
     while True:
         if isSelect and last_result_time and (asyncio.get_event_loop().time() - last_result_time > 2):
             executeSelectAI()
             isSelect = False
+            isNarrate = False
+            isShowSQL = False
+            isRunSQL = False
+            isExplainSQL = False
         await asyncio.sleep(1)
 
 
@@ -152,30 +180,45 @@ def authenticator():
     private_key = oci.signer.load_private_key_from_file(config["key_file"])
     auth = SecurityTokenSigner(token=token, private_key=private_key)
     return auth
+
+
 def executeSelectAI():
-    global cummulativeResult, isInsertResults, latest_thetime, latest_question, latest_answer
+    global cummulativeResult, isNarrate, isShowSQL, isRunSQL, isExplainSQL, isInsertResults, latest_thetime, latest_question, latest_answer
     print(f"executeSelectAI called cummulative result: {cummulativeResult}")
+    cummulativeResult += " . Answer in 20 words or less."
+    if isNarrate:
+        selectai_action = "narrate" # query database and give back narration style reply
+    elif isShowSQL:
+        selectai_action = "showsql" # show the sql generated by select AI for this prompt/query
+    elif isRunSQL:
+        selectai_action = "runsql" # show the results of running the sql generated by select AI for this prompt/query
+    elif isExplainSQL:
+        selectai_action = "explainsql" # explain the sql generated by select AI for this prompt/query
+    else:
+        selectai_action = "chat"  # bypass database and go straight to LLM
 
     query = """SELECT DBMS_CLOUD_AI.GENERATE(
                 prompt       => :prompt,
-                profile_name => 'openai_gpt35', 
-                action       => 'chat')
+                profile_name => 'AIHOLO', 
+                action       => :action)
             FROM dual"""
 
     try:
         with connection.cursor() as cursor:
-            cursor.execute(query, {'prompt': cummulativeResult})
+            print(f"executeSelectAI using {selectai_action}")
+            cursor.execute(query, {'prompt': cummulativeResult, 'action': selectai_action})
             result = cursor.fetchone()
             if result and isinstance(result[0], oracledb.LOB):
                 text_result = result[0].read()
                 print(f"Query result: {text_result}")
-
                 latest_thetime = datetime.now()
                 latest_question = cummulativeResult
                 latest_answer = text_result[:3000]
 
             cummulativeResult = ""
 
+            if selectai_action in ("showsql", "runsql", "explainsql"):
+                return
             # API key-based authentication...
             config = oci.config.from_file("~/.oci/config", "DEFAULT")
             speech_client = AIServiceSpeechClient(config)
@@ -184,13 +227,13 @@ def executeSelectAI():
                 text=f" {latest_answer}",
                 is_stream_enabled=False,
                 configuration=oci.ai_speech.models.TtsOracleConfiguration(
-                model_family="ORACLE",
-                # Brian Annabelle Bob Stacy Phil Cindy Brad
-                model_details=oci.ai_speech.models.TtsOracleTts2NaturalModelDetails(voice_id="Brian"),
-                speech_settings=oci.ai_speech.models.TtsOracleSpeechSettings(
-                speech_mark_types=["WORD"]
-            ),
-        )
+                    model_family="ORACLE",
+                    # Brian Annabelle Bob Stacy Phil Cindy Brad
+                    model_details=oci.ai_speech.models.TtsOracleTts2NaturalModelDetails(voice_id="Brian"),
+                    speech_settings=oci.ai_speech.models.TtsOracleSpeechSettings(
+                        speech_mark_types=["WORD"]
+                    ),
+                )
             )
 
             response = speech_client.synthesize_speech(synthesize_speech_details=text_to_speech)
@@ -206,6 +249,7 @@ def executeSelectAI():
     except Exception as e:
         print(f"An error occurred: {e}")
 
+
 async def handle_request(request):
     global latest_thetime, latest_question, latest_answer
     data = {
@@ -214,6 +258,7 @@ async def handle_request(request):
         "answer": latest_answer
     }
     return web.json_response(data)
+
 
 if __name__ == "__main__":
     loop = asyncio.new_event_loop()  # Fix event loop issue
@@ -237,7 +282,7 @@ if __name__ == "__main__":
     )
 
     # Instance, resource principal, or session token-based authentication (as shown below) can also be used
-    # client = AIServiceSpeechClient(
+    # client = RealtimeClient(
     #     config=config,
     #     realtime_speech_parameters=realtime_speech_parameters,
     #     listener=SpeechListener(),
