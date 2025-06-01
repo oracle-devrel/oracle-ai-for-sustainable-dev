52dde0b437059d38bcc4b94b1e58ecdf13501479
paulparkinson <paul.parkinson@gmail.com>
2025-06-01 19:11:33 -0400

    financial app work

---
+++ financial/true-cache-stock-ticker/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+## Copyright (c) 2025 Oracle and/or its affiliates.
+## Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl/
+
+#kubectl apply -f  backend-springboot-service-ClusterIP.yaml  -n financial
+kubectl delete deployment backend-springboot  -n financial
+
+cp globallydistributeddatabase-springboot-deployment_template.yaml globallydistributeddatabase-springboot-deployment.yaml
+sed -i '' "s|IMAGE_PLACEHOLDER|$IMAGE|g" globallydistributeddatabase-springboot-deployment.yaml
+kubectl apply -f globallydistributeddatabase-springboot-deployment.yaml -n financial
+
