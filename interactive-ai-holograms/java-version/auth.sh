17442a932922c898ae8415ff508045139730a1d9
paulparkinson <paul.parkinson@gmail.com>
2025-06-09 15:39:48 -0400

    financial app work

--- interactive-ai-holograms/java-version/auth.sh
+++ interactive-ai-holograms/java-version/auth.sh
@@ -3,5 +3,3 @@
 # token itself expires after 1 hour, but it is automatically refreshed as long as the stored credentials remain valid.
 # provides long-lived authentication (~1 week) via Application Default Credentials (ADC).
 gcloud auth application-default login
-mvn clean package
-java -jar .\target\oracleai-0.0.1-SNAPSHOT.jar
