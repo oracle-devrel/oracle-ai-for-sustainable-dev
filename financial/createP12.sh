754248e7605fbef2b4e8f28b7ef0f8e2f2dd7959
Paul Parkinson <paul.parkinson@oracle.com>
2025-07-14 16:09:34 -0400

    updates

--- financial/createP12.sh
+++
@@ -1,4 +0,0 @@
-
-
- openssl pkcs12 -export -in oracledatabase-financial.org.cert  -inkey  oracledatabase-financial.key  -out oracledatabasefinancialorg.p12 -name oracledatabasefinancialorg
- cp oracledatabasefinancialorg.p12 src/main/resources/
